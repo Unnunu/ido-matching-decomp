1256b84ea228c8400ec25f28e667dc80ce236b91
LLONSIT <76184758+LLONSIT@users.noreply.github.com>
2026-03-24 05:22:10 -0500

    Match ugen/foldconst and ugen/translate (#58)

--- src/ugen/fold.h
+++ src/ugen/fold.h
@@ -1,4 +1,6 @@
 #ifndef FOLD_H
 #define FOLD_H
 function is_constant(arg0: pointer): boolean; external;
+function fold(arg0: Ptree): pointer; external;
+function fold1(arg0: Ptree): boolean; external;
 #endif /* FOLD_H */
--- src/ugen/symbol.h
+++ src/ugen/symbol.h
@@ -2,4 +2,5 @@
 #define SYMBOL_H
 procedure output_entry_point(arg0: integer); external;
 function get_sym_kind(arg0: integer): integer; external;
+function get_mtag(arg0: integer): integer; external;
 #endif
